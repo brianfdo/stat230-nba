4f382eb4bbc88dec851456c1e1b93e93c8b9ea26
amashiana <82017857+amashiana@users.noreply.github.com>
2025-05-13 17:18:21 -0700

    Add files via upload

--- WLS.R
+++ WLS.R
@@ -1,4 +1,3 @@
-library(ggplot2)
 
 #helper function for stratified train test split
 # we want a sufficient number of "stars" in both train and test, and we define a star as players with any accolades
@@ -105,8 +104,6 @@ print(summary(abs(exp(pred)-exp(yts_adv))))
 
 rsq_test(pred, yts_adv)
 
-plot(fitted(ols_box), resid(ols_box), xlab = "Fitted Values", ylab = "Residuals", main = "Residuals vs Fitted Values, OLS")
-abline(h = 0, col = "black")
 
 # WLS
 
@@ -165,73 +162,11 @@ ints$name=c("DeMar DeRozan", "James Harden", "Kentavious Caldwell-Pope", "LeBron
 player_2024[top_FA_i, c(2,64)]
 
 ints$actual=c(16.96825, 17.33164, 16.94038, 17.70178, 15.60727, 17.71152, 17.04891, 14.55149, 16.10805, 17.37505)
-library(ggplot2)
 
 
 
-ggplot(ints, aes(x = as.factor(name))) +
-  
-  # WLS prediction interval
-  geom_pointrange(aes(y = wls_fit, ymin = wls_lwr, ymax = wls_upr, color = "WLS Interval"),
-                  position = position_nudge(x = -0.2)) +
-  
-  # OLS prediction interval
-  geom_pointrange(aes(y = ols_fit, ymin = ols_lwr, ymax = ols_upr, 
-                      color = "OLS Interval"),
-                  position = position_nudge(x = 0.2)) +
-  
-  # Actual log salaries
-  geom_point(aes(y = actual, color = "Actual Salary"),
-             size = 2) +
-  
-  # Manual legend mappings
-  scale_color_manual(name = "Legend", 
-                     values = c("WLS Interval" = "red", 
-                                "OLS Interval" = "blue", 
-                                "Actual Salary" = "black")) +
-  
-  # Labels and theme
-  labs(title = "WLS and OLS Confidence Intervals and Actual 2024 log(Salaries), Selected Top Free Agents",
-       y = "log(Salary)",
-       x = "Player Name") +
-  theme_minimal() +
-  scale_y_continuous(limits = c(13.7, 20.2))+
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-#Checking assumptions for OLS, WLS, LASSO
-
-#OLS
-#linear relationship check
-plot(fitted(ols_box), resid(ols_box), xlab="OLS Fitted Vals", ylab="OLS Residuals", main="OLS Fitted Vals vs Residuals")
-abline(h = 0, col = "red")
-
-plot(fitted(ols_adv), resid(ols_adv), xlab="OLS Fitted Vals", ylab="OLS Residuals", main="OLS Fitted Vals vs Residuals, Advanced Stats")
-abline(h = 0, col = "red")
-
-# independent errors
-library(lmtest)
-dwtest(ols_box)
-dwtest(ols_adv)
-#p>0.05, assumption holds
-
-#multicolinearity check
-library(car)
-vif(ols_box)
-vif(ols_adv)
-
-#homoskedastic errors check
-qqnorm(residuals(ols_box))
-qqline(residuals(ols_box))
-
-qqnorm(residuals(ols_adv))
-qqline(residuals(ols_adv))
-
-#Lower tail deviates from the line significantly: suggests heteroskedastic errors
-
-# WLS
-#Same assumptions as OLS, we are checking 
-qqnorm(residuals(wls_box))
-qqline(residuals(wls_box))
-
+saveRDS(ols_box, file = "ols_box.rds")
+saveRDS(ols_adv, file = "ols_adv.rds")
+write.csv(ints, "ints.csv")
 
